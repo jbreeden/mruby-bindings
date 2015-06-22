9bdc0394836d8edae4d1a0471c612cfd04833810
Jared Breeden <jared.breeden@gmail.com>
2015-06-21 20:18:27 -0400

    Omitting nanomsg bindings from mruby build for now

--- mruby-1.1.0/build_config.rb
+++ mruby-1.1.0/build_config.rb
@@ -9,9 +9,9 @@ MRuby::Build.new('host') do |conf|
   conf.gembox "full-core"
 
   conf.gem "../mrbgems/mruby-apr"
-  conf.gem "../mrbgems/mruby-nanomsg"
+  #conf.gem "../mrbgems/mruby-nanomsg"
   configure_mruby_apr(conf)
-  configure_mruby_nanomsg(conf)
+  #configure_mruby_nanomsg(conf)
 
   if ENV['DEBUG'] && (ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR'])
     conf.cc.flags << "/DEBUG"
