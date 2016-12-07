da77eb05be01245aad9aa0818aeeecc146a33aff
Jared Breeden <jared.breeden@gmail.com>
2016-12-06 21:33:24 -0800

    Restrict the indexes/repos api

--- lib/mruby/bindings/indexes.rb
+++ lib/mruby/bindings/indexes.rb
@@ -2,35 +2,69 @@ require 'csv'
 require 'mruby/bindings'
 
 module MRuby::Bindings
+
+  # Data shall be stored in "indexes" of "repos."
+  # Indexes shall respond to methods, indicating names of repos.
+  # Repos shall respond to "each," and yield hashes.
+  # There shall be no "rewind," for one day our inputs may be streamed to us.
+  # Also, why would you need to look at a thing twice? Make up your mind already.
+  # So the interface is written, so shall it be.
+
+  class CSVRepo
+    include Enumerable
+
+    def initialize(csv)
+      @csv = csv
+    end
+
+    def each(*args)
+      if block_given?
+        @csv.each(*args) do |e|
+          yield e.to_h
+        end
+      else
+        enum_for :each
+      end
+    end
+  end
+
   class CSVDirectoryIndex
+    # Class macro for defining the directory to expose.
     def self.directory(name=nil)
       return @directory if name.nil?
       @directory = name
     end
 
+    def directory
+      self.class.directory
+    end
+
     def initialize
-      files = Dir[self.class.directory + '/*.csv']
-      @lookup_tables = files.reduce({}) do |acc, cur|
-        current_table = acc[File.basename(cur).sub('.csv', '')] = []
-        CSV.foreach(cur, headers: true) do |row|
-          current_table.push(row)
-        end
-        acc
-      end
+      @files = {}
     end
 
     def method_missing(name)
       name_str = name.to_s
-      hyphenated_name = name_str.sub('_', '-')
-      if (name_str_minus_q = name_str[/(.*)\?$/, 1])
-        @lookup_tables.has_key? name_str_minus_q
-      elsif @lookup_tables.keys.include?(name_str)
-        @lookup_tables[name_str] 
-      elsif @lookup_tables.keys.include?(hyphenated_name)
-        @lookup_tables[hyphenated_name] 
-      else
-        super
+
+      hit = @files[name_str]
+      return hit if hit
+
+      filename_str = name_str.gsub('_', '-')
+      filename_str = filename_str.end_with?('?') ? filename_str[0..-2] : filename_str
+
+      path = "#{self.directory}/#{filename_str}.csv"
+
+      path_exists = File.exists?(path)
+
+      if name_str.end_with?('?')
+        return path_exists
       end
+
+      if path_exists
+        return @files[name] = CSVRepo.new(CSV.open(path, headers: true))
+      end
+
+      super
     end
   end
 
