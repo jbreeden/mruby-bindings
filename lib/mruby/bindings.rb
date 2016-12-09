17ec9aa1c5625b79c756b7491420f3b2a475dc59
Jared Breeden <jared.breeden@gmail.com>
2016-12-09 09:22:37 -0800

    Add YAMLDirectoryIndex

--- lib/mruby/bindings.rb
+++ lib/mruby/bindings.rb
@@ -1,20 +1,5 @@
 module MRuby
   module Bindings
-  end
-end
-require "mruby/bindings/version"
-require "mruby/bindings/names"
-require "mruby/bindings/type_checks"
-require "mruby/bindings/typedef_resolver"
-require "mruby/bindings/ctypes"
-require "mruby/bindings/ctypes_definition"
-require "mruby/bindings/builtin_ctypes"
-require "mruby/bindings/hooks"
-require "mruby/bindings/indexes"
-require "mruby/bindings/rendered_text"
-
-module MRuby
-  module Bindings
     module Paths
       GEM_ROOT = File.absolute_path(File.dirname(__FILE__) + "/../../")
 
@@ -27,13 +12,26 @@ module MRuby
 
       INGEST_DIR = File.absolute_path("#{OUTPUT_DIR}/ingested-declarations")
       DISCOVERY_DIR = File.absolute_path("#{OUTPUT_DIR}/discovery")
+      SUMMARY_DIR = File.absolute_path("#{OUTPUT_DIR}/summary")
       BLUEPRINT_DIR = File.absolute_path("#{OUTPUT_DIR}/blueprint")
       RENDER_DIR = File.absolute_path("#{OUTPUT_DIR}/render")
 
-      RESULT_DIR = File.absolute_path("#{OUTPUT_DIR}/#{Hooks.gem_name}")
+      RESULT_DIR = File.absolute_path("#{OUTPUT_DIR}/mrbgem")
       SRC_RESULT_DIR = File.absolute_path("#{RESULT_DIR}/src")
       INCLUDE_RESULT_DIR = File.absolute_path("#{RESULT_DIR}/include")
       GENERATE_RESULT_DIR = File.absolute_path("#{RESULT_DIR}/include")
     end
   end
 end
+
+require "mruby/bindings/version"
+require "mruby/bindings/names"
+require "mruby/bindings/type_checks"
+require "mruby/bindings/typedef_resolver"
+require "mruby/bindings/ctypes"
+require "mruby/bindings/ctypes_definition"
+require "mruby/bindings/builtin_ctypes"
+require "mruby/bindings/hooks"
+require "mruby/bindings/indexes"
+require "mruby/bindings/rendered_text"
+
--- lib/mruby/bindings/indexes.rb
+++ lib/mruby/bindings/indexes.rb
@@ -1,16 +1,94 @@
 require 'csv'
+require "yaml"
 require 'mruby/bindings'
 
 module MRuby::Bindings
 
-  # Data shall be stored in "indexes" of "repos."
-  # Indexes shall respond to methods, indicating names of repos.
-  # Repos shall respond to "each," and yield hashes.
+  # Data shall be stored in "indexes" of "collections."
+  # Indexes shall respond to methods, indicating names of collections.
+  # Collections shall respond to "each," and yield hashes.
   # There shall be no "rewind," for one day our inputs may be streamed to us.
   # Also, why would you need to look at a thing twice? Make up your mind already.
   # So the interface is written, so shall it be.
+  
+  module SendAncestors
+    def send_ancestors(m, *args, &block)
+      ancestors[1..-1].each do |a|
+        if a.respond_to?(m)
+          return a.send(m, *args, &block)
+        end
+      end
+      raise NoMethodError.new("No ancestor responds to #{m}")
+    end
+  end
 
-  class CSVRepo
+  module DirectoryIndex
+    module ClassMethods
+      include SendAncestors
+
+      # Class macro for defining the directory to expose.
+      def directory(*args)
+        if args.empty?
+          return @directory || send_ancestors(:directory)
+        end
+        @directory = args[0]
+      end
+
+      # Class macro for defining the file extension to support.
+      def ext(*args)
+        if args.empty?
+          return @ext || send_ancestors(:ext)
+        end
+        @ext = args[0]
+      end
+    end
+
+    module InstanceMethods
+      def directory
+        self.class.directory
+      end
+
+      def ext
+        self.class.ext
+      end
+
+      def initialize
+        @files = {}
+      end
+
+      def method_missing(name)
+        name_str = name.to_s
+
+        hit = @files[name_str]
+        return hit if hit
+
+        filename_str = name_str.gsub('_', '-')
+        filename_str = filename_str.end_with?('?') ? filename_str[0..-2] : filename_str
+
+        path = "#{self.directory}/#{filename_str}.#{self.ext}"
+        puts path
+
+        path_exists = File.exists?(path)
+
+        if name_str.end_with?('?')
+          return path_exists
+        end
+
+        if path_exists
+          return @files[name] = self.materialize(path)
+        end
+
+        super
+      end
+    end
+
+    def self.included(other)
+      other.send(:include, InstanceMethods)
+      other.send(:extend, ClassMethods)
+    end
+  end
+
+  class CSVCollection
     include Enumerable
 
     def initialize(csv)
@@ -29,50 +107,46 @@ module MRuby::Bindings
   end
 
   class CSVDirectoryIndex
-    # Class macro for defining the directory to expose.
-    def self.directory(name=nil)
-      return @directory if name.nil?
-      @directory = name
+    include DirectoryIndex
+    ext 'csv'
+
+    def materialize(path)
+      CSVCollection.new(CSV.open(path, headers: true))
+    end
+  end
+
+  class YAMLCollection
+    include Enumerable
+
+    def initialize(file)
+      @file = file
     end
 
-    def directory
-      self.class.directory
+    def each
+      YAML.load_stream(@file).each { |obj|
+        yield obj
+      }
     end
+  end
 
-    def initialize
-      @files = {}
-    end
+  class YAMLDirectoryIndex
+    include DirectoryIndex
+    ext 'yaml'
 
-    def method_missing(name)
-      name_str = name.to_s
-
-      hit = @files[name_str]
-      return hit if hit
-
-      filename_str = name_str.gsub('_', '-')
-      filename_str = filename_str.end_with?('?') ? filename_str[0..-2] : filename_str
-
-      path = "#{self.directory}/#{filename_str}.csv"
-
-      path_exists = File.exists?(path)
-
-      if name_str.end_with?('?')
-        return path_exists
-      end
-
-      if path_exists
-        return @files[name] = CSVRepo.new(CSV.open(path, headers: true))
-      end
-
-      super
+    def materialize(path)
+      YAMLCollection.new(File.open(path))
     end
   end
 
   class DiscoveryIndex < CSVDirectoryIndex
-    directory './mruby-bindings.out/discovery'
+    directory MRuby::Bindings::Paths::DISCOVERY_DIR
   end
 
   class SummaryIndex < CSVDirectoryIndex
-    directory './mruby-bindings.out/summary'
+    directory MRuby::Bindings::Paths::SUMMARY_DIR
+  end
+
+  class RenderIndex < YAMLDirectoryIndex
+    directory MRuby::Bindings::Paths::RENDER_DIR
   end
 end
